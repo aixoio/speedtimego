ca82c8e1e8115ed4a14735d2340278363cb70f31
aixoio <79969530+aixoio@users.noreply.github.com>
2023-09-20 11:13:57 +1000

    Add code

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	
+	if len(os.Args) <= 1 {
+		fmt.Println("Usage: <command>")
+		return
+	}
+
+	fmt.Println("\nStarting...\n")
+
+	starttime := time.Now().UnixNano()
+
+	cmddat := os.Args[1:]
+
+	cmdName := cmddat[0]
+	cmdArgs := cmddat[1:]
+
+	cmd := exec.Command(cmdName, cmdArgs...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	cmd.Run()
+
+	endtime := time.Now().UnixNano()
+
+	fmt.Println("\nEnding...\n")
+
+	time := endtime - starttime
+
+	fmt.Printf("Command ran in %d nanoseconds!\n", time)
+	fmt.Printf("Command ran in %d milliseconds!\n", time / 1000000)
+	fmt.Printf("Start time: %d unix nanoseconds\n", starttime)
+	fmt.Printf("End time: %d unix nanoseconds\n", endtime)
+
 }
